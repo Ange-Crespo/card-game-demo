edb2ce2dff37f76986797199fd5ca8d9845e014a
ClementLL <clement.laurlevesque@gmail.com>
2024-02-28 14:45:49 +0100

    Added routes

--- card-game/src/App.tsx
+++ card-game/src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HeaderComponent } from './component'
+import { DeckBuilder, Game, Home } from './pages';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Router>
+        <HeaderComponent/>
+        <Routes>
+          <Route path='' element={<Home/>}/>
+          <Route path='/deckbuilder' element={<DeckBuilder/>}/>
+          <Route path='/play' element={<Game/>}/>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ card-game/src/pages/index.tsx
@@ -0,0 +1,3 @@
+export * from './DeckBuilder'
+export * from './Home'
+export * from './Game'
