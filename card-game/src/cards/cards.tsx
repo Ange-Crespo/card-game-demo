bf851f195fc05a093f5efc674d9d3cd5f3cdca6f
Ange Crespo <ange.crespo@hotmail.fr>
2024-02-28 16:01:40 +0100

    Add Debug for Cards

--- card-game/src/cards/cards.tsx
+++ card-game/src/cards/cards.tsx
@@ -1,4 +1,4 @@
-[
+export var cards = [
     {
         "id": 1,
         "name": "Flame Phoenix",
--- card-game/src/component/Card.tsx
+++ card-game/src/component/Card.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { ICard } from "../interface/interface";
 import "../card.css"
 
