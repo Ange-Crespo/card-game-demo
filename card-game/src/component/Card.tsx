10e4398e044b7180371bdeb52be8e69a3a1e419a
Ange Crespo <ange.crespo@hotmail.fr>
2024-02-28 16:01:40 +0100

    Add Debug for Cards

--- card-game/src/component/Card.tsx
+++ card-game/src/component/Card.tsx
@@ -1,23 +1,20 @@
-import * as React from "react";
+import React from "react";
 import { ICard } from "../interface/interface";
 import "../card.css"
 
-export default function Card() {
-    const newCard : ICard = { 
-        id: Math.random(),
-        name: "pikachu",
-        force: 3,
-        hp: 120,
-        cost: 5,
-        point: 4 
-      }
+interface Props {
+    card: ICard
+}
+
+export const Card: React.FC<Props> = (props:Props) => {
+    
     return (
         <div className="card">
-            <span className="name">{newCard.name}</span>
-            <span className="force">{newCard.force}</span>
-            <span className="hp">{newCard.hp}</span>     
-            <span className="cost">{newCard.cost}</span>
-            <span className="point">{newCard.point}</span>
+            <span className="name">{props.card.name}</span>
+            <span className="force">{props.card.force}</span>
+            <span className="hp">{props.card.hp}</span>     
+            <span className="cost">{props.card.cost}</span>
+            <span className="point">{props.card.point}</span>
         </div>
     );
 }
--- card-game/src/pages/Debug.tsx
+++ card-game/src/pages/Debug.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Card } from '../component/Card'
+import { ICard } from "../interface/interface";
+import { cards } from "../cards/cards";
+
+const newCard : ICard = { 
+    id: Math.random(),
+    name: "Raichu",
+    force: 3,
+    hp: 120,
+    cost: 5,
+    point: 4 
+  }
 
 export const Debug = () => {
+    const updateNewCard = ()=>{
+        return cards[Math.random()*200];
+    };
   return (
-    <div>DeBuG o_O'</div>
-  )
+    <><button onClick={()=>updateNewCard()}>Click to Update Card</button><div>DeBuG o_O'</div><Card card={newCard} /></>
+  );
 }
